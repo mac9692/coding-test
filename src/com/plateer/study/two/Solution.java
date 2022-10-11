85347d4b0f869b8004eb1d459bc7f7dbf27561c5
1328384 <mac9692@nate.com>
2022-10-11 17:48:26 +0900

    20221011 two 성공

--- src/com/plateer/study/two/Solution.java
+++ src/com/plateer/study/two/Solution.java
@@ -11,16 +11,13 @@ public class Solution {
     }
 
     public static int[] solution(String[] id_list, String[] report, int k) {
-        // 신고 생성
         Set<String> set = new LinkedHashSet<>(Arrays.asList(report));
 
-        // 신고 카운트 생성
         Map<String, Integer> map = new LinkedHashMap<>();
         for (String id : id_list) {
             map.put(id, 0);
         }
 
-        // 신고 카운트 집계
         set.forEach(s -> {
             String[] strings = s.split("\\s");
             if (map.containsKey(strings[1])) {
@@ -28,11 +25,9 @@ public class Solution {
             }
         });
 
-        // 신고자 명단 생성
         List<String[]> list = new ArrayList<>();
         set.forEach(s -> list.add(s.split("\\s")));
 
-        // 메일 발송 명단 생성
         Map<String, Integer> mailMap = new LinkedHashMap<>();
         for (String id : id_list) {
             mailMap.put(id, 0);
@@ -41,27 +36,19 @@ public class Solution {
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             if (entry.getValue() >= k) {
                 list.forEach(reportMember -> {
-                    if (reportMember[1].contains(entry.getKey())){
+                    if (reportMember[1].equals(entry.getKey())){
                         mailMap.replace(reportMember[0], mailMap.get(reportMember[0])+1);
                     }
                 });
             }
         }
 
-        // 배열 생성
         int[] answer = new int[mailMap.size()];
 
         int index = 0;
         for (Integer integer : mailMap.values()) {
             answer[index++] = integer;
         }
-
-        // 눈에 잘보이게..
-        System.out.println("신고자 명단 : " + set);
-        System.out.println("신고 카운트 : " + map);
-        System.out.println("신고자 명단 : " + list);
-        System.out.println("메일 발송 명단 : " + mailMap);
-        System.out.println(mailMap.values());
 
         return answer;
     }
