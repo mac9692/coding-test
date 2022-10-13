0ed9b4f6ecd528e0c1aea9ca8834b2483993824c
1328384 <mac9692@nate.com>
2022-10-13 16:46:28 +0900

    20221013 day3 성공

--- src/com/plateer/study/day3/Solution.java
+++ src/com/plateer/study/day3/Solution.java
@@ -1,24 +1,35 @@
 package com.plateer.study.day3;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 // 최소직사각형 - https://school.programmers.co.kr/learn/courses/30/lessons/86491
 public class Solution {
     public static void main(String[] args) {
-        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+//        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+//        int[][] sizes = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+        int[][] sizes = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
+
         solution(sizes);
     }
     public static int solution(int[][] sizes) {
-        List<int[]> intArr = Arrays.stream(sizes).distinct().collect(Collectors.toList());
+        List<Integer> bigInts = new ArrayList<>();
+        List<Integer> smallInts = new ArrayList<>();
 
-        intArr.stream().forEach(ints -> {
-            System.out.println(ints[0]);
-            System.out.println(ints[1]);
-        });
+        for (int i = 0; i < sizes.length; i++) {
+            if (Integer.compare(sizes[i][0], sizes[i][1]) > 0) {
+                bigInts.add(sizes[i][0]);
+                smallInts.add(sizes[i][1]);
+            } else {
+                bigInts.add(sizes[i][1]);
+                smallInts.add(sizes[i][0]);
+            }
+        }
 
-        int answer = 0;
-        return answer;
+        Collections.sort(bigInts, Collections.reverseOrder());
+        Collections.sort(smallInts, Collections.reverseOrder());
+
+        return bigInts.get(0) * smallInts.get(0);
     }
 }
