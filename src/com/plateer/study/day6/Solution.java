bde517dfe1444844a2b295bcd62ca96b118cd9a8
1328384 <mac9692@nate.com>
2022-10-18 10:06:42 +0900

    20221018 day6 도전

--- src/com/plateer/study/day6/Solution.java
+++ src/com/plateer/study/day6/Solution.java
@@ -8,9 +8,20 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) {
+/*
         int[] fees = {180, 5000, 10, 600};
-        String[] records = {"05:34 5961 입차", "06:00 0000 입차", "06:34 0000 출차", "07:59 5961 출차", "07:59 0148 입차",
-                "18:59 0000 입차", "19:09 0148 출차", "22:59 5961 입차", "23:00 5961 출차"};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN",
+                "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+*/
+
+        int[] fees = {120, 0, 60, 591};
+        String[] records = {"16:00 3961 IN", "16:00 0202 IN", "18:00 3961 OUT", "18:00 0202 OUT", "23:58 3961 IN"};
+/*
+        int[] fees = {180, 5000, 10, 600};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN",
+                "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+*/
+
         solution(fees, records);
     }
     public static int[] solution(int[] fees, String[] records) {
@@ -21,14 +32,14 @@ public class Solution {
         Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < recordsList.size(); i++) {
-            if ("입차".equals(recordsList.get(i).getGbn()) && "출차".equals(recordsList.get(i + 1).getGbn())) {
+            if ("IN".equals(recordsList.get(i).getGbn()) && "OUT".equals(recordsList.get(i + 1).getGbn())) {
                 if (map.containsKey(recordsList.get(i).getCarNumber())) {
                     map.replace(recordsList.get(i).getCarNumber(), (int) (map.get(recordsList.get(i).getCarNumber()) + ChronoUnit.MINUTES.between(recordsList.get(i).getTime(), recordsList.get(i + 1).getTime())));
                 } else {
                     map.put(recordsList.get(i).getCarNumber(), (int) ChronoUnit.MINUTES.between(recordsList.get(i).getTime(), recordsList.get(i + 1).getTime()));
                 }
                 i++;
-            } else if ("입차".equals(recordsList.get(i).getGbn()) && "입차".equals(recordsList.get(i + 1).getGbn())) {
+            } else if ("IN".equals(recordsList.get(i).getGbn()) && "IN".equals(recordsList.get(i + 1).getGbn())) {
                 if (map.containsKey(recordsList.get(i).getCarNumber())) {
                     map.replace(recordsList.get(i).getCarNumber(), (int) (map.get(recordsList.get(i).getCarNumber()) + ChronoUnit.MINUTES.between(recordsList.get(i).getTime(), LocalTime.of(23, 59))));
                 } else {
@@ -42,27 +53,26 @@ public class Solution {
 
         List<Integer> valueList = new ArrayList<>();
         for (String key : keyList) {
-            System.out.println("key : " + key + ", value : " + map.get(key));
+//            System.out.println("key : " + key + ", value : " + map.get(key));
             valueList.add(map.get(key));
         }
 
         int[] answer = new int[valueList.size()];
 
         for (int i = 0; i < valueList.size(); i++) {
-            if (i <= fees[0]) {
+            if (valueList.get(i) <= fees[0]) {
                 answer[i] = fees[1];
                 break;
             } else {
-                answer[i] = fees[1];
-                while (i % fees[3] > 0) {
-                    answer[i] += fees[4];
-                    if (i % fees[3] < fees[4]) {
-                        break;
-                    }
+                int chargeTime = 0;
+                if (((valueList.get(i) - fees[0]) % fees[2]) != 0) {
+                    chargeTime = ((valueList.get(i) - fees[0] + fees[2]) / fees[2]);
+                } else {
+                    chargeTime = ((valueList.get(i) - fees[0]) / fees[2]);
                 }
+                answer[i] = fees[1] + (chargeTime * fees[3]);
             }
         }
-
         System.out.println(Arrays.toString(answer));
         return answer;
     }
