d21214a74af100f0d2d91922f8f9c3a5fa9b79c1
1328384 <mac9692@nate.com>
2022-10-19 15:57:59 +0900

    20221019 day8 성공

--- src/com/plateer/study/day8/Solution.java
+++ src/com/plateer/study/day8/Solution.java
@@ -1,4 +1,20 @@
 package com.plateer.study.day8;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// 최댓값과 최솟값 - https://school.programmers.co.kr/learn/courses/30/lessons/12939
 public class Solution {
+    public static void main(String[] args) {
+        String s = "1 2 3 4";
+        solution(s);
+    }
+
+    public static String solution(String s) {
+        List<Integer> integerList = Arrays.stream(s.split(" ")).map(Integer::parseInt).sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        String answer = String.valueOf(integerList.get(integerList.size()-1)).concat(" ").concat(String.valueOf(integerList.get(0)));
+        return answer;
+    }
 }
