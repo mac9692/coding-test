19b5e2d61b453bc331f80f4b62754cb1a7eb7632
1328384 <mac9692@nate.com>
2022-10-24 17:28:16 +0900

    20221024 day10 성공

---
+++ src/com/plateer/study/day10/SolutionThree.java
@@ -0,0 +1,30 @@
+package com.plateer.study.day10;
+
+import java.util.Arrays;
+
+// 문자열 내 p와 y의 개수 - https://school.programmers.co.kr/learn/courses/30/lessons/12916
+public class SolutionThree {
+    public static void main(String[] args) {
+        String s = "pPoooyY";
+
+    }
+    static boolean solution(String s) {
+        String[] strings = Arrays.stream(s.split("")).toArray(String[]::new);
+        int countP = 0;
+        int countY = 0;
+
+        for (String str : strings) {
+            if ("p".equalsIgnoreCase(str)) {
+                countP++;
+            } else if ("y".equalsIgnoreCase(str)) {
+                countY++;
+            }
+        }
+
+        if (countP == countY || (countP == 0 && countY == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
