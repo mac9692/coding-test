a613290d17274f44a7c424985601af546e219da7
parkjinseong <mac9692@nate.com>
2022-10-28 23:57:09 +0900

    20221028 day14 성공

---
+++ src/com/plateer/study/day14/Solution.java
@@ -0,0 +1,23 @@
+package com.plateer.study.day14;
+
+// 부족한 금액 계산하기 - https://school.programmers.co.kr/learn/courses/30/lessons/82612
+public class Solution {
+    public static void main(String[] args) {
+        int price = 3;
+        int money = 20;
+        int count = 5;
+        solution(price, money, count);
+    }
+    public static long solution(int price, int money, int count) {
+        long answer = 0;
+        long sum = 0;
+        for (int i = 1; i <= count; i++) {
+            sum += i * price;
+        }
+
+        if (sum > money) {
+            answer = sum - money;
+        }
+        return answer;
+    }
+}
