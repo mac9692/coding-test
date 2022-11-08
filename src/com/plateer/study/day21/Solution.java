1cbd205af531d8bcb2c95c5d1a3fd4da8ef4425d
parkjinseong <mac9692@nate.com>
2022-11-08 21:15:16 +0900

    20221108 day21 성공

---
+++ src/com/plateer/study/day21/Solution.java
@@ -0,0 +1,19 @@
+package com.plateer.study.day21;
+
+import java.util.Scanner;
+
+// 직사각형 별찍기 - https://school.programmers.co.kr/learn/courses/30/lessons/12969
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
